521885c7743fe0d3f0dfbd2f7ba3ebd946a1429f
Thomas Almeida <thomasfa86@hotmail.com>
2020-12-24 14:34:52 -0300

    Define with_file method

--- lib/xplor/explore.rb
+++ lib/xplor/explore.rb
@@ -26,5 +26,17 @@ class Explore
   end
 
   def self.with_file(input)
+    lines = File.readlines(input).map { |line| line.delete!("\n") }
+    height = lines.shift.split(' ').map(&:to_i)
+
+    response = []
+    (0..lines.size - 1).step(2).each do |index|
+      current_position, current_pointer = Vehicle.get_position_pointer(lines[index])
+
+      commands = lines[index + 1].chars
+      response << Vehicle.run_commands(commands, current_position, current_pointer, height)
+    end
+
+    response.join("\n")
   end
 end
