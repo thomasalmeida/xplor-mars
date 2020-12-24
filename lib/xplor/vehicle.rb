1c07e6300af95349de3521a58307ed26408bb1c8
Thomas Almeida <thomasfa86@hotmail.com>
2020-12-24 14:33:17 -0300

    Create Vehicle class

---
+++ lib/xplor/vehicle.rb
@@ -0,0 +1,19 @@
+class Vehicle
+  MOVEMENTS = { 'N': [0, 1], 'E': [1, 0], 'S': [0, -1], 'W': [-1, 0] }.freeze
+  POINTER = ['N', 'E', 'S', 'W'].freeze
+
+  def self.get_position_pointer(value)
+  end
+
+  def self.run_commands(commands, position, pointer, height)
+  end
+
+  def self.action(command, position, pointer)
+  end
+
+  def self.move(position, pointer)
+  end
+
+  def self.rotate(direction, pointer)
+  end
+end
