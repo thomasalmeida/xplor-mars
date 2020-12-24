bb65a9b631b8bdd9d0d38e056af2964b0a64624e
Thomas Almeida <thomasfa86@hotmail.com>
2020-12-24 14:34:34 -0300

    Define without_file method

--- lib/xplor/explore.rb
+++ lib/xplor/explore.rb
@@ -10,6 +10,19 @@ class Explore
   private
 
   def self.without_file
+    puts 'Define maximum height. Example: 5 5'
+    height = gets.chomp
+    height = height.split(' ').map(&:to_i)
+
+    puts 'Define initial positiom and @pointer. Example: 1 2 N'
+    current = gets.chomp
+    current_position, current_pointer = Vehicle.get_position_pointer(current)
+
+    puts 'Define list of commands. Example: LMLMLMLMM'
+    commands = gets.chomp
+    commands = commands.chars
+
+    Vehicle.run_commands(commands, current_position, current_pointer, height)
   end
 
   def self.with_file(input)
